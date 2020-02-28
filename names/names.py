f68f2f4c8fc78b2ab64965e3b2d16295a268fb58
Anna Winther <anski.anna@gmail.com>
2020-02-28 09:05:22 +0000

    Better solution for names

--- names/names.py
+++ names/names.py
@@ -21,12 +21,23 @@ duplicates = []  # Return the list of duplicates in this data structure
 # This runs in 7.124 seconds on my computer. It's runtime complexity is therefore O(n^2) --> its polynomial, so when the input size increases, the runtime or space used will grow at a faster rate. Not a great solution as it's not scalable.
 
 # NEW SOLUTION 
-# initialize a bst using the Binary Search Tree created earlier this week, passing in the value as the first item in names_1
-# for each name in namaes_1 insert it into the bst
-# for each name in nmaes_2
-    # if the bts contains the name
-        # use duplicates.append(name)
 
+# initialize a bst using the Binary Search Tree created earlier this week, passing in the value as the first item in names_1
+bst = BinarySearchTree(names_1[0])
+# for each name in namaes_1 
+for name in names_1:
+    # insert it into the bst
+    bst.insert(name)
+
+# for each name in nmaes_2
+for name in names_2:
+    # if the bts contains the name
+    if bst.contains(name):
+        # use duplicates.append(name)
+        duplicates.append(name)
+
+# using this solution runs in 0.10642290115356445 seconds
+# uses O(n) --> its linear
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
