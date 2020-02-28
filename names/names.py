a3c303d99a1fc45a1aa5ebc69dd01354039fbdfc
Anna Winther <anski.anna@gmail.com>
2020-02-28 08:58:56 +0000

    Found riuntime complexity of the names algorithm

--- names/names.py
+++ names/names.py
@@ -14,10 +14,14 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:                        n * (what's inside of the loop)         O(n)
+#     for name_2 in names_2:                        n * (what's inside of the loop)         O(n)
+#         if name_1 == name_2:                          constant                                O(1)
+#             duplicates.append(name_1)                 constant                                O(1)
+# This runs in 7.124 seconds on my computer. It's runtime complexity is therefore O(n^2) --> its polynomial, so when the input size increases, the runtime or space used will grow at a faster rate. Not a great solution as it's not scalable.
+
+# NEW SOLUTION 
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
